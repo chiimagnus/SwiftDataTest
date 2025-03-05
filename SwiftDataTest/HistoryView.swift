cdc8734b82d601fccc2e17cc57aedd34e3552b65
chiimagnus <chii_magnus@outlook.com>
2025-03-05 20:19:18 +0800

    -

---
+++ SwiftDataTest/HistoryView.swift
@@ -0,0 +1,18 @@
+//
+//  HistoryView.swift
+//  SwiftDataTest
+//
+//  Created by chii_magnus on 2025/3/5.
+//
+
+import SwiftUI
+
+struct HistoryView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HistoryView()
+}
---
+++ SwiftDataTest/NewItemView.swift
@@ -0,0 +1,18 @@
+//
+//  NewItemView.swift
+//  SwiftDataTest
+//
+//  Created by chii_magnus on 2025/3/5.
+//
+
+import SwiftUI
+
+struct NewItemView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NewItemView()
+}
